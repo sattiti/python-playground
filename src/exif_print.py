b31cc3e3679897340b39c97106cc708a9fb6a40d
sattiti <sattiti@users.noreply.github.com>
2019-10-20 20:54:48 +0900

    update

--- src/exif_print.py
+++ src/exif_print.py
@@ -3,9 +3,12 @@
 import sys
 import io
 import traceback
+import codecs
+import PIL.Image
 from PIL import Image
 from PIL.ExifTags import TAGS, GPSTAGS
 import requests
+import photos
 
 def std_encoding(charset):
   try:
@@ -18,14 +21,29 @@ def std_encoding(charset):
     # traceback.print_exc()()
     pass
 
+
+def print_exif(exif):
+  for k, v in exif.items():
+    print("{}: {}".format(TAGS.get(k), v))
+
+
+def get_exif_from_photos():
+  a = photos.pick_asset()
+  return a.get_image()._getexif()
+
+
+def get_exif_from_url(img_url):
+  c = requests.get(img_url)
+  img = Image.open(io.BytesIO(c.content))
+  return img._getexif()
+
+
 if __name__ == '__main__':
   std_encoding('utf-8')
+  try:
+    img = get_exif_from_photos()
+    print_exif(img)
+  except Exception as e:
+    traceback.print_exc()()
   
-  img_path = ''
-  
-  c = requests.get(img_path)
-  img = Image.open(io.BytesIO(c.content))
-  exif = img._getexif()
-  for id, val in exif.items():
-    tag = TAGS.get(id, id), val
-    print(tag)
+  # img_url = ''
